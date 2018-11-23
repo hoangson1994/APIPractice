1ce9d211dff31c93ed7ccecf43114dbd856b2bf6
Hoang Son <sonhtd00496@fpt.edu.vn>
2018-11-23 11:24:46 +0700

    new

---
+++ APIThi/Data/APIThiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIThi.Models
+{
+    public class APIThiContext : DbContext
+    {
+        public APIThiContext (DbContextOptions<APIThiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIThi.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ APIThi/Migrations/20181123030812_initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIThi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Salary = table.Column<long>(nullable: false),
+                    Department = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
